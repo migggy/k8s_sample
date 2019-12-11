5a0669b72e75dafc2cf5877c76ad3743328f47f6
migggy <migggy@users.noreply.github.com>
2019-12-11 15:08:21 +0900

    fix metrice server

---
+++ kind/README.md
@@ -0,0 +1,6 @@
+# kind
+
+https://kind.sigs.k8s.io/docs/user/quick-start/
+
+## refs
+- [kind で HPA をしてみる](https://qiita.com/tksarah/items/84b740bbfef4e0fc4f63)
--- kind/init-cluster.sh
+++ kind/init-cluster.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Need following operation.
-# Intall helm(v3.0 >), kind(v0.6.1 >) and go(v1.13 >) in advance
-# Add helm repo.
+# - Requermentes
+#   - helm(v3.0 >)
+#   - kind(v0.6.1 >)
+#   - go(v1.13 >)
+# - Add helm repo.
 #   - helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 
 cd `dirname $0`
 
 # create cluster for local pc
 kind create cluster --name kind --config multi-node-cluster.yaml --image kindest/node:v1.15.0
-kubectl cluster-info --context kind-kind
+kubectl apply -f https://gist.githubusercontent.com/hjacobs/69b6844ba8442fcbc2007da316499eb4/raw/5b8678ac5e11d6be45aa98ca40d17da70dcb974f/kind-metrics-server.yaml
+helm repo update
 
 # install cluster component
-helm repo update
-helm install stable/metrics-server --name-template metrics-server --namespace kube-system
