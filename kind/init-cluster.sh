f4d88653554b384ee2448d55a1eafc936467f5c5
migggy <migggy@users.noreply.github.com>
2019-12-12 14:48:12 +0900

    ini cluster

--- kind/init-cluster.sh
+++ kind/init-cluster.sh
@@ -11,6 +11,6 @@ cd `dirname $0`
 # create cluster for local pc
 kind create cluster --name kind --config multi-node-cluster.yaml --image kindest/node:v1.15.0
 kubectl apply -f https://gist.githubusercontent.com/hjacobs/69b6844ba8442fcbc2007da316499eb4/raw/5b8678ac5e11d6be45aa98ca40d17da70dcb974f/kind-metrics-server.yaml
-helm repo update
 
-# install cluster component
+# install cluster component by helm
+helm repo update
